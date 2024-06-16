a97e60b48341bd53564c16c9a830021e5deeb204
Victor Martinez <correo.vmp@gmail.com>
2024-06-16 11:28:39 +0200

    1. Allow for configurable bert tokenizer to preprocess the data. 2. Drop raw columns to save memory and time

--- src/data_pipeline/etl/etl_pipeline.py
+++ src/data_pipeline/etl/etl_pipeline.py
@@ -9,24 +9,26 @@ from src.data_pipeline.preprocessing_pipeline import PreprocessingPipeline
 logger = utils.get_logger(__name__)
 
 
-def run(input_path: str, output_path: str, fraction=1.0):
+def run(input_path: str, output_path: str, bert_model_name = "bert-base-uncased", fraction = 1.0, seed = 42):
     """
     Full ETL pipeline including extraction, transformation, and loading.
 
     Args:
         input_path (str): Path to the input JSONL.gz file.
         output_path (str): Path to save the output Parquet file.
+        bert_model_name (str): Name of the BERT model to use for tokenization. Defaults to "bert-base-uncased".
         fraction (float): Fraction of the data to sample. Defaults to 1.
+        seed (int): Random seed for reproducibility. Defaults to 42.
     """
     logger.info("Starting ETL pipeline.")
 
     # Step 1: Extract
     logger.info("Extracting data.")
-    raw_df = from_jsonl_gz_file(input_path, fraction)
+    raw_df = from_jsonl_gz_file(input_path, fraction, seed)
 
     # Step 2: Transform
     logger.info("Transforming data.")
-    preprocessing_pipeline = PreprocessingPipeline()
+    preprocessing_pipeline = PreprocessingPipeline(bert_model_name=bert_model_name)
     transformed_data = preprocessing_pipeline.preprocess(raw_df)
 
     # Step 3: Load

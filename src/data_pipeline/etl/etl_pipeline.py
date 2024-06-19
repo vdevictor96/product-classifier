084b8958ef1852ba6881ad803a4033e0b59e97ed
Victor Martinez <correo.vmp@gmail.com>
2024-06-19 16:40:10 +0200

    Remove etl imports from preprocessing_pipeline to be reusable in inference

--- src/data_pipeline/etl/etl_pipeline.py
+++ src/data_pipeline/etl/etl_pipeline.py
@@ -1,23 +1,21 @@
 """Module that performs the ETL pipeline (Extract, Transform, Load)."""
 import fire
-from src.data_pipeline.etl.extract import from_jsonl_gz_file
-from src.data_pipeline.etl.load import to_parquet_file
-
+import src.data_pipeline.etl.extract as extract
+import src.data_pipeline.etl.transform as transform
+import src.data_pipeline.etl.load as load
 from src.data_pipeline import utils
-from src.data_pipeline.preprocessing_pipeline import PreprocessingPipeline
 
 
 logger = utils.get_logger(__name__)
 
 
-def run(input_path: str, output_path: str, bert_model_name = "bert-base-uncased", fraction = 1.0, seed = 42):
+def run(input_path: str, output_path: str, fraction=1.0, seed=42):
     """
     Full ETL pipeline including extraction, transformation, and loading.
 
     Args:
         input_path (str): Path to the input JSONL.gz file.
         output_path (str): Path to save the output Parquet file.
-        bert_model_name (str): Name of the BERT model to use for tokenization. Defaults to "bert-base-uncased".
         fraction (float): Fraction of the data to sample. Defaults to 1.
         seed (int): Random seed for reproducibility. Defaults to 42.
     """
@@ -25,16 +23,15 @@ def run(input_path: str, output_path: str, bert_model_name = "bert-base-uncased"
 
     # Step 1: Extract
     logger.info("Extracting data.")
-    raw_df = from_jsonl_gz_file(input_path, fraction, seed)
+    raw_df = extract.from_jsonl_gz_file(input_path, fraction, seed)
 
     # Step 2: Transform
     logger.info("Transforming data.")
-    preprocessing_pipeline = PreprocessingPipeline(bert_model_name=bert_model_name)
-    transformed_data = preprocessing_pipeline.preprocess(raw_df)
+    transformed_data = transform.preprocess(raw_df)
 
     # Step 3: Load
     logger.info("Loading data.")
-    to_parquet_file(transformed_data, output_path)
+    load.to_parquet_file(transformed_data, output_path)
 
     logger.info("ETL pipeline completed.")
 
--- src/data_pipeline/preprocessing_pipeline.py
+++ src/data_pipeline/preprocessing_pipeline.py
@@ -1,8 +1,7 @@
 """Module for preprocessing the product data for BERT-based NLP tasks."""
 from pyspark.sql import DataFrame
-from transformers import AutoTokenizer
-
-from src.data_pipeline.etl import clean, transform
+from pyspark.sql.functions import col, udf
+from pyspark.sql.types import StringType
 from src.data_pipeline import utils
 
 logger = utils.get_logger(__name__)
@@ -12,39 +11,41 @@ class PreprocessingPipeline:
     """
     A reusable class to preprocess product data for BERT-based NLP tasks.
 
-    Attributes:
-        tokenizer (BertTokenizer): The BERT tokenizer.
-        ordered_columns_to_keep (list): List of column names to keep in the cleaned data.
-
     Methods:
         preprocess(df: DataFrame) -> DataFrame:
             The entire pipeline including cleaning and text processing.
     """
 
-    def __init__(self, bert_model_name: str = "bert-base-uncased"):
+    def __init__(self):
         """
         Initializes the preprocessing pipeline with a BERT tokenizer.
 
-        Args:
-            bert_model_name (str): Name of the BERT model to use for tokenization.
         """
-        self.tokenizer = AutoTokenizer.from_pretrained(bert_model_name)
         self.ordered_columns_to_keep = [
-            "title", "description", "feature", "brand", "asin", "main_cat"]
+            "title", "description", "feature", "brand", "main_cat"]
+        self.combined_text_column = "combined_text"
 
-    def _clean_data(self, df: DataFrame) -> DataFrame:
+    def _clean_data(self, df: DataFrame, ordered_columns_to_keep=None) -> DataFrame:
         """
-        Cleans the data.
+        Clean the input DataFrame by removing unnecessary columns and handling missing values.
 
         Args:
-            df (DataFrame): Input dataframe with product data.
-
+            df (DataFrame): Input DataFrame.
+            ordered_columns_to_keep (list, optional): List of columns to keep in the DataFrame in the desired order. 
+                                                  If None, keep all columns in their original order.
         Returns:
-            DataFrame: Cleaned dataframe.
+            DataFrame: Cleaned DataFrame.
         """
-        return clean.clean_data(df, self.ordered_columns_to_keep)
+        # Ensure "ordered_columns_to_keep" is a subset of the actual DataFrame columns
+        if ordered_columns_to_keep is not None:
+            actual_columns = df.columns
+            ordered_columns_to_keep = [
+                col for col in ordered_columns_to_keep if col in actual_columns]
+            df = df.select(*ordered_columns_to_keep)
 
-    def _process_text(self, df: DataFrame) -> DataFrame:
+        return df
+
+    def _process_text(self, df: DataFrame, combined_text_column="combined_text") -> DataFrame:
         """
         Process the text with BERT tokenization.
 
@@ -54,12 +55,32 @@ class PreprocessingPipeline:
         Returns:
             DataFrame: DataFrame with tokenized text column.
         """
-        df = transform.combine_text_fields(df)
-        df.show(2, truncate=False)
-        df = transform.bert_tokenize_text(df, self.tokenizer)
+        df = self._combine_text_fields(df, combined_text_column)
         # Drop the original columns
-        columns_to_keep = ["input_ids", "attention_mask", "main_cat"]
+        columns_to_keep = [combined_text_column, "main_cat"]
         return df.select(*columns_to_keep)
+
+    def _combine_text_fields(self, df: DataFrame, combined_text_column="combined_text") -> DataFrame:
+        """
+        Combines text fields into a single field for model input.
+
+        Args:
+            df (DataFrame): Input DataFrame.
+            combined_text_column (str, optional): Name of the new column with the combined text. Defaults to "combined_text".
+
+        Returns:
+            DataFrame: DataFrame with combined text field.
+        """
+
+        # Define a UDF to combine all columns into a single string
+        combine_udf = udf(
+            lambda *cols: " ".join([str(col) for col in cols if col is not None]), StringType())
+
+        # Get all column names from the DataFrame except the label column "main_cat"
+        column_names = [c for c in df.columns if c != "main_cat"]
+
+        # Apply the UDF to combine all columns
+        return df.withColumn(combined_text_column, combine_udf(*[col(c) for c in column_names]))
 
     def preprocess(self, df: DataFrame) -> DataFrame:
         """
@@ -72,9 +93,9 @@ class PreprocessingPipeline:
             DataFrame: Preprocessed dataframe ready for training or inference.
         """
         logger.info("Cleaning the data.")
-        cleaned_df = self._clean_data(df)
+        cleaned_df = self._clean_data(df, self.ordered_columns_to_keep)
         logger.info("Processing the text data.")
-        preprocessed_df = self._process_text(cleaned_df)
+        preprocessed_df = self._process_text(
+            cleaned_df, self.combined_text_column)
         logger.info("Preprocessing pipeline completed.")
         return preprocessed_df
-
